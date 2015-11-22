5bc632cb279e09a058574dbe49dd50a362be162d
mattstyles <matt@veryfizzyjelly.com>
2015-11-22 21:52:59 +0000

    #remove packages from git

--- packages/core/src/constants/err.js
+++
@@ -1,8 +0,0 @@
-/**
- * Creates custom errors and error heirarchies
- */
-
-import { create } from 'errno'
-
-export const DispatchError = create( 'DispatchError' )
-export const StateError = create( 'StateError' )
